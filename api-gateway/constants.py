c6383f3cb368f47977685c87bb74d3fc3f8f1851
Daniel <danielorp@outlook.com>
2025-02-01 18:01:06 -0300

    improve pod names

--- api-gateway/constants.py
+++ api-gateway/constants.py
@@ -1,6 +1,6 @@
 from decouple import config
 
 KEYCLOAK_SERVER_URL = config("KEYCLOAK_SERVER_URL", default="http://msa-services-keycloak.msa-services.svc.cluster.local")
-REALM_NAME = config("REALM_NAME", default="tms-realm")
+REALM_NAME = config("REALM_NAME", default="msa-realm")
 CLIENT_ID = config("CLIENT_ID", default="fastapi-app")
 CLIENT_SECRET = config("CLIENT_SECRET", default="q42ynHIOrQZYxSHysRXIxCzWkmD5P3i9")
